210689c31982ae193acce621e0923ecfd2a773f9
Brayan Esteves <brayan.esteves93@gmail.com>
2025-02-10 11:34:41 -0400

    Clustering with K-MEANS 'iris.csv' in 'iris.controller.php' - Machine Learning in PHP [10/02/2025]

--- iris.controller.php
+++ iris.controller.php
@@ -17,10 +17,22 @@
     $classification->train($dataset->getTrainSamples(), $dataset->getTrainLabels());
     $predicted = $classification->predict($dataset->getTestSamples());
 
+    $clustering = new \Phpml\Clustering\KMeans(3);
+    $clusters   = $clustering->cluster($data->getSamples(), $dataset->getTrainLabels());    
+
     // Evaluating machine learning models.
-    
     $accuracy = \Phpml\Metric\Accuracy::score($dataset->getTestLabels(), $predicted);
     echo "Accuracy is : " . $accuracy . PHP_EOL;
+
+    // var_dump($clusters);
+    $file = fopen('cluster_data.csv', 'w');
+    foreach($clusters as $key => $cluster) {
+        foreach($cluster as $data) {
+            $dataToWrite = [...$data, $key];
+            fputcsv($file, $dataToWrite);
+        }
+    }
+    fclose($file);
 
     // Making predictions with trained models.
 
